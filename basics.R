fe83cd4354943ffd102aaa3f196bc9d44b3aa9ac
Yvonne Zhou <yifanyz@umich.edu>
2019-12-01 23:07:31 -0500

    Update basics.R

--- basics.R
+++ basics.R
@@ -39,3 +39,31 @@ sparse.rowMeans <- function(x,zero.omit = FALSE,na.rm=FALSE,dims=1,sparseResult=
     return (Matrix::rowSums(x,na.rm,dims,sparseResult)/Matrix::rowSums(abs(sign(x)),na.rm,dims,sparseResult))
   }
 }
+
+# Covariance for Matrix object (accepts both dense and sparse input, although dense matrix should probably use cov() from base)
+sparse.covariance <- function(x){
+  n = nrow(x)
+  cSums = colSums(x)
+  cMeans = colMeans(x)
+  covariance = tcrossprod(cMeans,(-2*cSums+n*cMeans))+as.matrix(crossprod(x))
+  return (covariance/(n-1))
+}
+
+## Correlation for Matrix object -- accepts both dense and sparse input, although dense matrix should probably use cov() from base
+## two approaches have almost identital time performance
+
+# Correlation approach 1
+sparse.correlation <- function(x) {
+  n = nrow(x)
+  cSums = colSums(x)
+  cMeans = colMeans(x)
+  covariance = tcrossprod(cMeans,(-2*cSums+n*cMeans))+as.matrix(crossprod(x))
+  correlation = covariance/crossprod(t(sqrt(diag(covariance))))
+  return (correlation)
+}
+
+# Correlation approach 2
+sparse.correlation2 <- function(x) {
+  covariance = sparse.covariance(x)
+  return (Matrix::cov2cor(covariance))
+}
